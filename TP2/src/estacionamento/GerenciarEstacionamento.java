210c06c1c82301f60b9fcbbd38cf4268355d6929
gabri <gabri@DESKTOP-UU15HGD>
2022-08-30 16:48:20 -0300

    MOD1

--- TP2/src/estacionamento/GerenciarEstacionamento.java
+++ TP2/src/estacionamento/GerenciarEstacionamento.java
@@ -1,5 +1,36 @@
 package estacionamento;
 
 public class GerenciarEstacionamento {
+	
+	// Vou passar atributos dessa classe
+	
+	String tipoDeEstacionamento,
+		   horaDeAbir,
+		   horaDeFechar;
+	int capacidade;
+	
+	public GerenciarEstacionamento(String tipoDeEstacionamento, String horaDeAbrir, String horaDeFechar, int capacidade) {
+		this.capacidade = capacidade;
+		this.horaDeAbir = horaDeAbrir;
+		this.horaDeFechar = horaDeFechar;
+		this.tipoDeEstacionamento = tipoDeEstacionamento;
+		
+	}
+	
+	public int getCapacidade() {
+		return capacidade;
+	}
 
+	public String getHoraDeAbrir() {
+		return horaDeAbir;
+	}
+	
+	public String getHoraDeFechar() {
+		return horaDeFechar;
+	}
+	
+	public String getTipoDeEstacionamento() {
+		return tipoDeEstacionamento;
+	}
+	
 }
