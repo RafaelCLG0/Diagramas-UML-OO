57e55794672bc690c81e3a1b31d8f9ae3251b959
Arthur Grandão de Mello <arthur.gdemello@gmail.com>
2022-09-05 11:48:16 -0300

    Calcular valor e contratante.

--- TP2/src/estacionamento/HorasFracao.java
+++ TP2/src/estacionamento/HorasFracao.java
@@ -17,29 +17,39 @@ public class HorasFracao extends Padrao {
 	public void setValorTotal(float valorTotal) {
 		this.valorTotal = valorTotal;
 	}
-
 	
-	public float calcularValor(int valor) throws ValorAcessoInvalidoException {
-		estacio.getValorFracao();
+	public float calcularValor(int valor, float desconto) throws ValorAcessoInvalidoException {
 		int	temp = calcularTempo();
-		if(temp < 60) {
-			if(45 < temp)
-			valor = estacio.getValorFracao() * 3;
-			
+		//int valor = estacio.getValorFracao();
+		//float desconto = estacio.getDescontoHora();
+		
+		float resultado = ((int) temp/15) * valor;
+		
+		if(temp >= 60) {
+			return (resultado - ( resultado * ((int) temp/60)*desconto));
 		}
-		return (Float) null;
+		
+		return resultado;
 	}
 	
-	public float calcularContratante(int valor, int contratante) {
-		return (Float) null;
+	public float calcularContratante(int valor, float desconto) throws ValorAcessoInvalidoException {
+		float contratante = calcularValor(valor, desconto) * estacio.contratante;
+		return contratante;
 	}
 
 	public int calcularTempo() throws ValorAcessoInvalidoException {
+		String dataDeEntrada = a.getDataEntrada();
+		String dataDeSaida = a.getDataSaida();
 		int	horaDeEntrada = converterHora(a.getHoraEntrada());
 		int horaDeSaida = converterHora(a.getHoraSaida());
-		int entra = horaDeEntrada;
-		int saida = horaDeSaida;
-		int temp = saida - entra;
+		int temp;
+		
+		if (dataDeEntrada != dataDeSaida) {
+			temp = ((24*60) - horaDeEntrada) + horaDeSaida; //24*60 = Meia-noite
+		}else {
+			temp = horaDeSaida - horaDeEntrada;
+		}
+		
 		return temp;
 	}
 
